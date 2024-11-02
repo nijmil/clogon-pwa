62c30d57690edd140e91a61e66643e6d394e9d5f
Amanda Lim <amandahjlim@gmail.com>
2024-11-02 19:38:55 +1000

    Add changes for styling

--- docs/myprogram.js
+++ docs/myprogram.js
@@ -7,12 +7,13 @@ if (myProgram.length > 0) {
     myProgram.forEach(workshop => {
         const workshopDiv = document.createElement('div');
         workshopDiv.className = 'workshop';
-        workshopDiv.innerHTML = `
+        workshopDiv.innerHTML = 
             <h3>${workshop.title}</h3>
             <p>Time: ${workshop.time}</p>
-        `;
+        ;
         myProgramList.appendChild(workshopDiv);
     });
 } else {
     myProgramList.innerHTML = '<p>You haven\'t added any workshops yet.</p>';
 }
+
